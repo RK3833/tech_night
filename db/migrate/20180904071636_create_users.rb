7aaf969c66a7fc04e40a8867a65538345eb62104
Eagle eye <eagle.eye.great.tower@gmail.com>
2018-09-04 00:26:39 -0700

    recreate user model

---
+++ db/migrate/20180904071636_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :uid, unique: true
+      t.integer :status, default: 1, null: false
+      t.text :profile_content
+      t.string :email, unique: true, null: false
+      t.string :password_digest, null: false
+      t.string :birthday
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_04_071636) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "uid"
+    t.integer "status", default: 1, null: false
+    t.text "profile_content"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
