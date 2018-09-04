c69ea915c638f870d08dca6fbf0e06680c384374
Eagle eye <eagle.eye.great.tower@gmail.com>
2018-09-04 00:32:12 -0700

    bundle exec annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  birthday        :string(255)
+#  email           :string(255)      not null
+#  name            :string(255)      not null
+#  password_digest :string(255)      not null
+#  profile_content :text(65535)
+#  status          :integer          default(1), not null
+#  uid             :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates name, presence: true, length: { minimum: 3 }
+  validates email: true
+  enum status: { admin: 1, speaker: 2, participant: 3 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  birthday        :string(255)
+#  email           :string(255)      not null
+#  name            :string(255)      not null
+#  password_digest :string(255)      not null
+#  profile_content :text(65535)
+#  status          :integer          default(1), not null
+#  uid             :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
