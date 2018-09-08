9774dc23982172f86e2f929d4c7ca7a8a6b7dd82
Yuichi Mase <mase@temona.co.jp>
2018-09-08 16:43:42 +0900

    resolved_conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root "events#index"
+=======
+  get 'users/index'
+>>>>>>> abd63bd... add bootstrap and slim
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
